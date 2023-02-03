796cdf4679cb0d5172e199ad38eb7bf828fa5e21
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-02-03 16:07:07 -0600

    images are working

--- src/main/java/com/example/NYTAPI/Models/Article.java
+++ src/main/java/com/example/NYTAPI/Models/Article.java
@@ -2,11 +2,11 @@ package com.example.NYTAPI.Models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
 import java.util.List;
+
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Article {
     private Long id;
     private String url;
@@ -20,4 +20,19 @@ public class Article {
 
     private List<Media> media;
     private String imageUrl;
+
+    public Article(Long id, String url, String section, String subsection, String byline, String title, String summary, List<Media> media) {
+        this.id = id;
+        this.url = url;
+        this.section = section;
+        this.subsection = subsection;
+        this.byline = byline;
+        this.title = title;
+        this.summary = summary;
+        this.media = media;
+        if (!media.isEmpty()) {
+            this.imageUrl = getMedia().get(0).getMediaMetadata().get(1).getUrl();
+        }
+    }
+
 }
